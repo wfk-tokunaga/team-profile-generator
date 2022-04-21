f0cde20d65b857e695f398e015ec2436e94867bd
wfk-tokunaga <wktokunaga@gmail.com>
2022-04-21 12:37:41 -0700

    created developer class

--- __tests__/Developer.test.js
+++ __tests__/Developer.test.js
@@ -1,1 +1,54 @@
-const Employee = require('../lib/Employee');
+const Developer = require('../lib/Developer');
+
+test("creates a new developer object", () => {
+    const name = 'eika';
+    const id = 0;
+    const email = 'wktokunaga@gmail.com';
+    const github = 'wfk-tokunaga';
+    const developer = new Developer(name, id, email, github);
+
+    expect(developer.name).toBe('eika');
+    expect(developer.email).toBe('wktokunaga@gmail.com');
+    expect(developer.id).toBe(0);
+    expect(developer.github).toBe('wfk-tokunaga');
+})
+
+test("gets name of developer", () => {
+    const name = 'eika';
+    const id = 0;
+    const email = 'wktokunaga@gmail.com';
+    const github = 'wfk-tokunaga';
+    const developer = new Developer(name, id, email, github);
+
+    expect(developer.getName()).toBe('eika');
+})
+
+test("gets id of developer", () => {
+    const name = 'eika';
+    const id = 0;
+    const email = 'wktokunaga@gmail.com';
+    const github = 'wfk-tokunaga';
+    const developer = new Developer(name, id, email, github);
+
+    expect(developer.getId()).toBe(0);
+})
+
+test("gets email of developer", () => {
+    const name = 'eika';
+    const id = 0;
+    const email = 'wktokunaga@gmail.com';
+    const github = 'wfk-tokunaga';
+    const developer = new Developer(name, id, email, github);
+
+    expect(developer.getEmail()).toBe('wktokunaga@gmail.com');
+})
+
+test("gets office of developer", () => {
+    const name = 'eika';
+    const id = 0;
+    const email = 'wktokunaga@gmail.com';
+    const github = 'wfk-tokunaga';
+    const developer = new Developer(name, id, email, github);
+
+    expect(developer.getGithub()).toBe('wfk-tokunaga');
+})
--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 const inquirer = require('inquirer');
+
+new Team().buildTeam();
